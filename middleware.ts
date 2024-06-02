2506a0a8f0c7daa14474d5af70bd89c9cd82a360
MaxKulakov <kulakovmaxim99@gmail.com>
2024-06-02 10:29:13 +0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,3 @@
 export { default } from 'next-auth/middleware'
 
-export const config = { matcher: ['/profile/:path*', '/hh', '/resume','/protected/:path*'] }
+export const config = { matcher: ['/profile/:path*', '/hh', '/resume','/protected/:path*', '/update-github/:path*'] }
