de04654928320b48fe19685841e22f0fef66f71f
MaxKulakov <kulakovmaxim99@gmail.com>
2024-06-04 23:38:25 +0400

    .

--- lib/getUserResume.tsx
+++ lib/getUserResume.tsx
@@ -3,7 +3,7 @@ export async function getUserResume(token: string) {
 		const response = await fetch('/api/hh?token=' + token);
 		const data = await response.json();
 		// console.log(data)
-		const resume_id = data.items[1].id;
+		const resume_id = data.items[0].id;
 		// const resume_experience = data.items[1].experience;
 
 		if (!data.error) {
